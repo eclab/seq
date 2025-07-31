eeb4f6a0569d3c9d77e9df0b97adca281387a010
Sean Luke <sean@cs.gmu.edu>
2025-07-31 14:12:18 +0200

    automaton drag and drop

--- seq/motif/automaton/gui/AutomatonUI.java
+++ seq/motif/automaton/gui/AutomatonUI.java
@@ -128,6 +128,8 @@ public class AutomatonUI extends MotifUI
  
     public void buildMenu()
         {
+        if (menu != null) return;		// already built, we don't want to make another one
+        
         menu = new JMenu("Automaton");
         JMenuItem load = new JMenuItem("Make Start Node");
         load.addActionListener(new ActionListener()
