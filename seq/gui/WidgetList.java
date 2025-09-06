537de642063596d63b8d424e651e10e854ceb29a
Sean Luke <sean@cs.gmu.edu>
2025-09-06 00:42:48 -0400

    comments

--- seq/gui/WidgetList.java
+++ seq/gui/WidgetList.java
@@ -7,29 +7,50 @@ package seq.gui;
 
 import java.awt.*;
 import javax.swing.*;
+
+/**
+	WIDGETLIST
+	
+	This is a simple component which displays other components vertically, each one with a label to their left,
+	as is often the case in object inspectors.  It is designed to easily add a border.  WidgetList itself uses
+	a BorderLayout with a single inner object located at BorderLayout.NORTH, so you can add additional objects
+	below it.
+**/
  
 public class WidgetList extends JPanel
     {
     private static final long serialVersionUID = 1;
-        
+    
+    // The actual container of components
     JPanel panel;
+    // The labels for the components
     JLabel[] labels;
+    // The components proper
     JComponent[] widgets;
     
+    /** Returns all the labels for the components.  Don't modify these. */
     public JLabel[] getLabels() { return labels; }
     
-    public WidgetList() { }
-                
+    /** Returns all the components.  Don't modify these. */
+    public JComponent[] getWidgets() { return widgets; }
+    
+    /** Sets WidgetList to have a border with the given label, or no border if the label is null. */
     public void makeBorder(String label)
         {
-        setBorder(new javax.swing.border.TitledBorder("<html><i>" + label + "</i></html>"));
+        if (label == null) setBorder(null);
+        else setBorder(new javax.swing.border.TitledBorder("<html><i>" + label + "</i></html>"));
         }
         
+    /** Constructs an empty WidgetList. */
+    public WidgetList() { }
+                
+    /** Constructs a WidgetList with the given components and their labels. */
     public WidgetList(String[] labels, JComponent[] widgets)
         {
         build(labels, widgets);
         }
         
+    /** Sets the WidgetList to display the given components and their labels. */
     public JPanel build(String[] labels, JComponent[] widgets)
         {
         this.labels = new JLabel[labels.length];
@@ -79,6 +100,8 @@ public class WidgetList extends JPanel
         return panel;
         }
 
+    /** If you have set tooltips for all the widgets, calling this method will cause their corresponding labels
+    	to adopt the same tooltips, which looks and feels more proper. */
     public void updateToolTips()
         {
         for(int i = 0; i < labels.length; i++)
