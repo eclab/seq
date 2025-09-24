a49e1f84b8570f5b65864a0dbcc1868659754574
Sean Luke <sean@cs.gmu.edu>
2025-09-24 17:41:53 -0400

    root parameter ccs

--- seq/gui/RootParameterList.java
+++ seq/gui/RootParameterList.java
@@ -24,10 +24,13 @@ public class RootParameterList extends JPanel
     SmallDial max;
     StringField seed;
     SmallDial[] dials = new SmallDial[Motif.NUM_PARAMETERS];
-        
+    SmallDial[] ccDials = new SmallDial[Motif.NUM_PARAMETERS];
+    JComboBox ccIn;
+    JPanel ccInPanel = new JPanel();
+
     public RootParameterList(Seq seq)
         {
-        JComponent[] comp = new JComponent[Motif.NUM_PARAMETERS + 3];
+        JComponent[] comp = new JComponent[Motif.NUM_PARAMETERS + 4];
         ReentrantLock lock = seq.getLock();
         lock.lock();
         try 
@@ -105,7 +108,7 @@ public class RootParameterList extends JPanel
                 final int _i = i;
                 dials[i] = new SmallDial(seq.getParameterValue(i))
                     {
-                    protected String map(double val) { return String.format("%.4f", val); }
+                    //protected String map(double val) { return String.format("%.4f", val); }
                     public double getValue() 
                         { 
                         ReentrantLock lock = seq.getLock();
@@ -121,11 +124,43 @@ public class RootParameterList extends JPanel
                         finally { lock.unlock(); }
                         }
                     };
-                comp[i + 3] = dials[i].getLabelledDial("0.0000  ");
+                }
+
+            for(int i = 0; i < Motif.NUM_PARAMETERS; i++)
+                {
+                final int _i = i;
+                ccDials[i] = new SmallDial(seq.getParameterCC(i) / 128.0)
+                    {
+                    protected String map(double val) { if (val == 1.0) return "None"; else return "" + (int)(val * 128.0); }
+                    public double getValue() 
+                        { 
+                        ReentrantLock lock = seq.getLock();
+                        lock.lock();
+                        try { return seq.getParameterCC(_i) / 128.0; }
+                        finally { lock.unlock(); }
+                        }
+                    public void setValue(double val) 
+                        { 
+                        ReentrantLock lock = seq.getLock();
+                        lock.lock();
+                        try { seq.setParameterCC(_i, (int)(val * 128.0)); }
+                        finally { lock.unlock(); }
+                        }
+                    };
+                JPanel combined = new JPanel();
+                combined.setLayout(new BorderLayout());
+                combined.add(dials[i].getLabelledDial("0.0000  "), BorderLayout.WEST);
+                combined.add(new JLabel("  CC "), BorderLayout.CENTER);
+                combined.add(ccDials[i].getLabelledDial("0.0000  "), BorderLayout.EAST);
+                comp[i + 3] = combined;
                 }
             }
         finally { lock.unlock(); }
-        String[] strs = new String[Motif.NUM_PARAMETERS + 3];
+        ccInPanel.setLayout(new BorderLayout());
+        rebuildCCIn(seq);
+        comp[comp.length - 1] = ccInPanel;
+        
+        String[] strs = new String[Motif.NUM_PARAMETERS + 4];
         strs[0] = "Rand Seed";
         strs[1] = "Rand Min";
         strs[2] = "Rand Max";
@@ -133,11 +168,11 @@ public class RootParameterList extends JPanel
             {
             strs[i + 3] = "Param " + (i + 1);
             }
+        strs[strs.length - 1] = "CC In";
         WidgetList list = new WidgetList(strs, comp);
         setLayout(new BorderLayout());
         DisclosurePanel panel = new DisclosurePanel("Root Arguments", list);
         add(panel, BorderLayout.CENTER);
-
 
         // tooltips
         comp[0].setToolTipText(RAND_SEED_TOOLTIP);
@@ -150,6 +185,43 @@ public class RootParameterList extends JPanel
             }
         list.updateToolTips();          // update the labels
         panel.setToolTipText(ROOT_ARGUMENTS_TOOLTIP);
+        }
+        
+    
+    public void rebuildCCIn(final Seq seq)
+        {
+        if (ccIn != null) ccInPanel.remove(ccIn);
+        
+        String[] ins = null;
+        ReentrantLock lock = seq.getLock();
+        lock.lock();
+        try
+            {
+            In[] seqIns = seq.getIns();
+            ins = new String[seqIns.length + 1];
+            ins[0] = "<html><i>None</i></html>";
+            for(int i = 0; i < seqIns.length; i++)
+                {
+                ins[i + 1] = "" + (i + 1) + ": " + seqIns[i].toString();
+                }
+            }
+        finally { lock.unlock(); }                              
+
+        ccIn = new JComboBox(ins);
+        ccIn.setSelectedIndex(seq.getParameterCCIn());
+        ccIn.addActionListener(new ActionListener()
+            {
+            public void actionPerformed(ActionEvent e)
+                {
+                if (seq == null) return;
+                ReentrantLock lock = seq.getLock();
+                lock.lock();
+                try { seq.setParameterCCIn(ccIn.getSelectedIndex()); }
+                finally { lock.unlock(); }                              
+                }
+            });
+                
+        ccInPanel.add(ccIn, BorderLayout.CENTER);
         }
     
     /*
